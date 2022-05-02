26aeae963ca6cbad37ba741708e37f8d0b980075
Margarita <margarita_anis@bk.ru>
2022-05-02 14:51:45 +0300

    Add HOMEWORK project Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите первое целое число ->");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("введите второе целое число ->");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+   Console.WriteLine(a + " больше " + b);
+}
+else
+{
+    Console.WriteLine(b + " больше " + a);
+}
