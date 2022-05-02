d82d91bd8e7c9844125369a0631d25ee85e28024
Margarita <margarita_anis@bk.ru>
2022-05-02 18:36:17 +0300

    Add HOMEWORK Task_8

---
+++ Task_8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите целое число ->");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+while (i <= N)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+    i++;
+}
+Console.ReadKey();
