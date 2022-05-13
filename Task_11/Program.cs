4128e2388aa41846f584255fdd64584caba2c589
Margarita <103448590+idCherry@users.noreply.github.com>
2022-05-13 22:12:33 +0300

    Delete Task_11 directory

--- Task_11/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿
-int number = new Random().Next(100, 1000);
-Console.WriteLine($"Number = {number}");
-int firstDigit = number / 100;
-int therdDigit = number % 10;
-Console.Write($"New Number = {firstDigit}{therdDigit}");
